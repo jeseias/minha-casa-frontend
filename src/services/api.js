54e7d5865cd05cc89f87f284e07e53c6296f80c0
jeseias <domingosjeseias@gmail.com>
2020-08-28 16:22:56 +0100

    Add link

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://127.0.0.1:8002/api/v1'
+  baseURL: true ? 'https://minhcasa.herokuapp.com/' :'http://127.0.0.1:8002/api/v1'
 });
