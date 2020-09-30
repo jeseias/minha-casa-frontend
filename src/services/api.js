e3b2d997fe9c398f0ddf5abe185b9e8b1f90625c
jeseias <domingosjeseias@gmail.com>
2020-09-30 19:43:24 +0100

    Changes Server url

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: true ? 'https://minhcasa.herokuapp.com/api/v1' : 'http://127.0.0.1:8002/api/v1'
+  baseURL: 'https://minhcasa.herokuapp.com/api/v1'  
 });
